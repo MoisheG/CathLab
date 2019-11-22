8af00696f2cd55462c74df86e10ceb703f97dbd4
Hobokiin <moishe.grosh@uoit.net>
2019-11-21 21:46:18 -0500

    Made a main menu, script needs implementation

---
+++ Assets/Menu/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManangement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        SceneManager.LoadScene("NEWLAB");
+	}
+	
+
+}
