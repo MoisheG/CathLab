d1e27ca469b93a8755f88647be7ec32937e1f09a
Hobokiin <moishe.grosh@uoit.net>
2019-11-27 20:05:02 -0500

    The Final Push. All assets implemented and tested. *NOT FINAL BUILD. FINAL BUILD IS IN A SEPARATE ZIP*

--- Assets/CATH LAB/Scripts/ScriptEvent.cs
+++ Assets/CATH LAB/Scripts/ScriptEvent.cs
@@ -10,6 +10,9 @@ public class ScriptEvent : MonoBehaviour
     public static float StageEvent = -1;
     public GameObject ECG;
     public GameObject HINK;
+    public GameObject DYK1;
+    public GameObject DYK2;
+    public GameObject DYK3;
     public AudioClip[] AQ;
     public static AudioSource AP;
     public bool[] AS = new bool[5];
@@ -19,7 +22,9 @@ public class ScriptEvent : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        DYK1.SetActive(false);
+        DYK2.SetActive(false);
+        DYK3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -41,10 +46,28 @@ public class ScriptEvent : MonoBehaviour
         if (StageEvent == 1)
         {
             ECG.SetActive(true);
+            DYK1.SetActive(true);
+        }
+        if(StageEvent == 2)
+        {
+            DYK1.SetActive(false);
+        }
+        if (StageEvent == 5)
+        {
+            DYK2.SetActive(true);
+        }
+        if (StageEvent == 6)
+        {
+            DYK2.SetActive(false);
         }
         if (StageEvent == 9)
         {
             HINK.SetActive(true);
+            DYK3.SetActive(true);
+        }
+        if (StageEvent == 10)
+        {
+            DYK3.SetActive(false);
         }
     }
 }
